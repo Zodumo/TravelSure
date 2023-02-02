131bf575241c276ba2609d93a967ea4a3f5635be
Treysuarez <treyxsuarez@gmail.com>
2023-02-02 20:02:49 +0000

    added travel api to javascript

--- js/script.js
+++ js/script.js
@@ -11,5 +11,19 @@ $.ajax({
 });
 
 
+// var city = "Kingston";
+var flydate = "2023-03-10";
+var travelURL = "https://app.goflightlabs.com/advanced-future-flights?access_key=" + travelKey +  "&type=departure&iataCode=BER&date=" + flydate;
+
+$.ajax({
+  url: travelURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
+
+
+
+
 
 
